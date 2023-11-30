2e16999b3538e9488b1c82a36c2ae059cc1e974d
NAVEENKUMAR2424 <90704299+NAVEENKUMAR2424@users.noreply.github.com>
2023-11-30 13:08:08 +0530

    Update Counter.js

--- src/Counter.js
+++ src/Counter.js
@@ -1,28 +1,37 @@
-// import React from "react";
-
-// class Counter extends React{
-//     render(){
-//         return(
-//             <div>
-//                 <p>Hijskkskkksk</p>
-//             </div>
-//         )
-//     }
-// }
-
-// export default Counter
 
 import React, { useState } from 'react'
 import './index.css'
+// import { array } from 'prop-types'
 
 const Counter = () => {
     const[count,hook]=useState(0)
+    const[color,setcolor]=useState("")
+
+    const handlehook=()=>{
+      hook(count+1);
+      if (count>=5){
+        hook(count+5);
+        const colors= `rgb(${Math.random() * 255},${Math.random() * 255},${Math.random() * 255})`;
+        setcolor(colors)     
+        document.body.style.backgroundColor=color;
+      }
+  
+
+    }
+
+   
   return (
-    <div className='Contain'>
+   
+    <div className='Contain'
+    style={{
+      backgroundColor: color
+    }}
+    > 
       <h1 className='Heading'>Counter</h1>
       <p>You clicked {count}</p>
-      <button onClick={()=>hook(count+1)}>Onclick</button>
+      <button onClick={handlehook}>Onclick</button>
     </div>
+   
   )
 }
 
